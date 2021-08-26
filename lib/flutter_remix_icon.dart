601ed2af52b4ebc9b1157d95e70b22bc8d6b1695
Ali Suleiman <allysuley@gmail.com>
2021-08-26 18:10:46 +0300

    Commit generated files

--- lib/flutter_remix_icon.dart
+++ lib/flutter_remix_icon.dart
@@ -1,13 +1,3 @@
-// import 'dart:async';
-//
-// import 'package:flutter/services.dart';
-//
-// class FlutterRemixIcon {
-//   static const MethodChannel _channel =
-//       const MethodChannel('flutter_remix_icon');
-//
-//   static Future<String?> get platformVersion async {
-//     final String? version = await _channel.invokeMethod('getPlatformVersion');
-//     return version;
-//   }
-// }
+library flutter_remix_icon;
+
+export 'remixicon.dart';
