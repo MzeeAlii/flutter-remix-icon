97e0c60a5e73f565ddd42a6de958851b40a9d09e
Ali Suleiman <allysuley@gmail.com>
2021-08-26 16:37:31 +0300

    Disable default plugin code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_remix_icon/flutte_remix_icon.dart';
+// import 'package:flutter_remix_icon/flutter_remix_icon.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,12 +27,12 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterRemixIcon.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+    // try {
+    //   platformVersion =
+    //       await FlutterRemixIcon.platformVersion ?? 'Unknown platform version';
+    // } on PlatformException {
+    //   platformVersion = 'Failed to get platform version.';
+    // }
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      // _platformVersion = platformVersion;
     });
   }
 
---
+++ lib/flutter_remix_icon.dart
@@ -0,0 +1,16 @@
+library flutter_remix_icon;
+
+// import 'dart:async';
+//
+// import 'package:flutter/services.dart';
+//
+// class FlutterRemixIcon {
+//   static const MethodChannel _channel =
+//       const MethodChannel('flutter_remix_icon');
+//
+//   static Future<String?> get platformVersion async {
+//     final String? version = await _channel.invokeMethod('getPlatformVersion');
+//     return version;
+//   }
+// }
+
