d7c7ca87bd21c3a1d1f49f931b5431158925b814
Ali Suleiman <allysuley@gmail.com>
2021-08-26 17:37:13 +0300

    Remove IconGenerator class
    Leaving only the functions. This makes the generator runnable since `main` function is reachable.

--- lib/icon_generator.dart
+++ lib/icon_generator.dart
@@ -4,102 +4,101 @@ import 'dart:io';
 
 // import 'package:flutter/services.dart';
 
-class IconGenerator {
-  final reservedKeys = ['class', 'new', 'null', 'sync', 'switch', 'try'];
-  final _className = 'RemixIcon';
+// class IconGenerator {
+final reservedKeys = ['class', 'new', 'null', 'sync', 'switch', 'try'];
+final _className = 'RemixIcon';
 
-  // To run this
-  void main() async {
-    Map? iconMap = await _getIconMap();
+// To run this
+void main() async {
+  Map? iconMap = await _getIconMap();
 
-    String fontCode = _buildCode(iconMap);
+  String fontCode = _buildCode(iconMap);
 
-    _writeCodeToFile(fontCode);
-  }
-
-  // Replace - with _
-  // Language reserved words: new -> new_icon
-  // Key starts with number: 24_hour -> i_24_hour
-  String _sanitizeKey(String key) {
-    key = key.replaceAll('-', '_');
-    var unsanitizedKey = key;
-
-    if (reservedKeys.contains(key)) {
-      key = key + '_icon';
-    }
-
-    if (key.startsWith(RegExp('[0-9]'))) {
-      key = 'i_' + key;
-    }
-
-    if (key != unsanitizedKey) {
-      print('converted: $unsanitizedKey -> $key');
-    }
-
-    return key;
-  }
-
-  // Replace &# with 0
-  String _sanitizeUnicode(String unicode) {
-    return unicode.replaceFirst('&#', '0');
-  }
-
-  // Extract glyph map from file
-  Future<Map>? _getIconMap() async {
-    // final String content =
-    //     await rootBundle.loadString('assets/fonts/remixicon-glyph.json');
-    String content = '';
-
-    await File('../assets/fonts/remixicon-glyph.json')
-        .openRead()
-        .transform(utf8.decoder)
-        .transform(new LineSplitter())
-        .forEach((line) => content += line);
-
-    return await json.decode(content);
-  }
-
-  // Make font class code from glyph map
-  String _buildCode(Map? iconMap) {
-    print('Building code...');
-    String code =
-        'import \'package:flutter/widgets.dart\'; class $_className {';
-
-    iconMap?.forEach((key, value) {
-      var _key = _sanitizeKey(key);
-      var _unicode = _sanitizeUnicode(value['unicode']);
-
-      code +=
-          'static const IconData $_key = IconData($_unicode, fontFamily: "$_className", fontPackage: "flutter_remix_icon");';
-    });
-    code += "}";
-    print('Building code complete.');
-
-    return code;
-  }
-
-  // Write code to local font class file
-  void _writeCodeToFile(String code) async {
-    print('Writing code...');
-    final fontFile = '$_className.dart';
-
-    await File(fontFile)
-        .writeAsString(code)
-        .then((file) => print('Writing font class code complete.'))
-        .onError((error, stackTrace) {
-      print('Writing font class code failed!');
-    });
-
-    final mainFile = 'flutter_remix_icon.dart';
-
-    code = 'library flutter_remix_icon;'
-        'export \'$_className.dart\';';
-
-    File(mainFile)
-        .writeAsString(code)
-        .then((file) => print('Writing code complete.'))
-        .onError((error, stackTrace) {
-      print('Writing code failed!');
-    });
-  }
+  _writeCodeToFile(fontCode);
 }
+
+// Replace - with _
+// Language reserved words: new -> new_icon
+// Key starts with number: 24_hour -> i_24_hour
+String _sanitizeKey(String key) {
+  key = key.replaceAll('-', '_');
+  var unsanitizedKey = key;
+
+  if (reservedKeys.contains(key)) {
+    key = key + '_icon';
+  }
+
+  if (key.startsWith(RegExp('[0-9]'))) {
+    key = 'i_' + key;
+  }
+
+  if (key != unsanitizedKey) {
+    print('converted: $unsanitizedKey -> $key');
+  }
+
+  return key;
+}
+
+// Replace &# with 0
+String _sanitizeUnicode(String unicode) {
+  return unicode.replaceFirst('&#', '0');
+}
+
+// Extract glyph map from file
+Future<Map>? _getIconMap() async {
+  // final String content =
+  //     await rootBundle.loadString('assets/fonts/remixicon-glyph.json');
+  String content = '';
+
+  await File('../assets/fonts/remixicon-glyph.json')
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(new LineSplitter())
+      .forEach((line) => content += line);
+
+  return await json.decode(content);
+}
+
+// Make font class code from glyph map
+String _buildCode(Map? iconMap) {
+  print('Building code...');
+  String code = 'import \'package:flutter/widgets.dart\'; class $_className {';
+
+  iconMap?.forEach((key, value) {
+    var _key = _sanitizeKey(key);
+    var _unicode = _sanitizeUnicode(value['unicode']);
+
+    code +=
+        'static const IconData $_key = IconData($_unicode, fontFamily: "$_className", fontPackage: "flutter_remix_icon");';
+  });
+  code += "}";
+  print('Building code complete.');
+
+  return code;
+}
+
+// Write code to local font class file
+void _writeCodeToFile(String code) async {
+  print('Writing code...');
+  final fontFile = '$_className.dart';
+
+  await File(fontFile)
+      .writeAsString(code)
+      .then((file) => print('Writing font class code complete.'))
+      .onError((error, stackTrace) {
+    print('Writing font class code failed!');
+  });
+
+  final mainFile = 'flutter_remix_icon.dart';
+
+  code = 'library flutter_remix_icon;'
+      'export \'$_className.dart\';';
+
+  File(mainFile)
+      .writeAsString(code)
+      .then((file) => print('Writing code complete.'))
+      .onError((error, stackTrace) {
+    print('Writing code failed!');
+  });
+}
+// }
