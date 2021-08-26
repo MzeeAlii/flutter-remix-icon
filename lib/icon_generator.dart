a66ad21f38d1e0b37f965031013bac1dbfa9dbcd
Ali Suleiman <allysuley@gmail.com>
2021-08-26 16:10:33 +0300

    Add function to sanitize unicode
    Replacing &# with 0 makes the character unicodes Dart compatible.

--- lib/icon_generator.dart
+++ lib/icon_generator.dart
@@ -11,6 +11,8 @@ class IconGenerator {
   main() async {
     Map? iconMap = await _getIconMap();
 
+    String fontCode = _buildCode(iconMap);
+
     // final filename = 'file.dart';
     // var file = await File(filename).writeAsString(code);
   }
@@ -37,20 +39,28 @@ class IconGenerator {
     return key;
   }
 
+  // Replace &# with 0
+  String _sanitizeUnicode(String unicode) {
+    return unicode.replaceFirst('&#', '0');
+  }
+
   Future<Map>? _getIconMap() async {
     final String response =
         await rootBundle.loadString('assets/fonts/remixicon-glyph.json');
     return await json.decode(response);
   }
 
-  String _buildCode(Map? iconMap){
-
+  String _buildCode(Map? iconMap) {
+    print('Building code...');
     String code =
         'import \'package:flutter/widgets.dart\'; class $_className {';
 
     iconMap?.forEach((key, value) {
+      var _key = _sanitizeKey(key);
+      var _unicode = _sanitizeUnicode(value['unicode']);
+
       code +=
-      'static const IconData ${_sanitizeKey(key)} = IconData($value[\'unicode\'], fontFamily: "$_className", fontPackage: "flutter_remix_icon");';
+          'static const IconData $_key = IconData($_unicode, fontFamily: "$_className", fontPackage: "flutter_remix_icon");';
     });
     code += "}";
     print('Code complete.');
